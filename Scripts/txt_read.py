2f6fc5f7509297ad87e33c4c29dfcbbc876c6b72
Eli Sulvarán Guel <elisulvaran@MacBook-Pro-de-Eli.local>
2019-11-03 13:40:44 -0600

    bueno

--- Scripts/txt_read.py
+++ Scripts/txt_read.py
@@ -9,5 +9,8 @@ parser.add_argument('--path', dest='myPath', required=True, help='Ingrese la rut
 #parser.add_argument('--outputpath', dest='outputPath', required=True,help='Ingrese la ruta del archivo de salida junto con su nombre y extension.')
 args=parser.parse_args
 
-for path,dirs,files,in os.walk(args.myPath):
-	print(dirs)
+for path,dirs,files in os.walk(args.myPath):  #for que recorre los directorios. 
+	for file in files:  #for que recorre los archivos. 
+		if file.endswith(".txt"):  #if que pregunta si el archivo es un .txt para evitar archivos no deseados. Si es verdadero, los archivos se abren.
+			with open(os.path.join(path,file), mode='r') as iFile:
+				print(iFile)
